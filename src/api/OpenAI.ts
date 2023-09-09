e304434c6ede1360b2e847a83ee0f9e27889d480
cooi0004 <cooi0004@student.monash.edu>
2023-09-09 23:16:23 +1000

    added prompt to gpt and tested with mock data

---
+++ src/api/OpenAI.ts
@@ -0,0 +1,55 @@
+import OpenAI from 'openai';
+import { Task } from '../types/types'
+import { GoogleEvent } from '../types/googleapitypes';
+import { ChatCompletionMessage } from 'openai/resources/chat';
+const openaikey = process.env.REACT_APP_OPENAI_API_KEY as string
+const openai = new OpenAI({ apiKey: openaikey, dangerouslyAllowBrowser: true });
+
+
+export async function sendGPT(tasks: Task[], events: GoogleEvent[]) {
+
+    const tasksTobeCompleted = JSON.stringify(tasks)
+    const scheduledEvents = JSON.stringify(events)
+    console.log(tasksTobeCompleted, scheduledEvents)
+    const taskPrompt: ChatCompletionMessage = {
+        role: 'user',
+        content: `Tasks needed to be complete ${tasksTobeCompleted}, Schduled Events: ${scheduledEvents}`
+
+    }
+    const systemPrompt: ChatCompletionMessage = {
+        role: 'system',
+        content: `You are are time manager, your are given a list of tasks
+        The tasks are in the following schema Task = {
+    title: string,
+    tag: string
+    urgency: 'High' | 'Medium' | 'Low'
+    due: Date
+    estimateTimeToComplete: number
+}, and a list of already scheduled events in the following schema Event={
+    summary: string
+    description?: string,
+    title: string,
+    notes: string,
+    start: {
+        dateTime: Date,
+        timeZone: string
+    }
+    end: {
+        dateTime: Date,
+        timeZone: string
+    }
+
+}. You are to schedule the tasks to fit into the schedule. To decide the most optimal schedule, take into account the due date, the urgency, and the estimated time to complete. 
+The events are already scheduled and cannot be changed. You are allowed to split a single task into multiple events on the schudule as long as the total time to complete the task is less than the estimated time to complete.
+Be sure to also take into account the person scheule, do not scheule task on unreasonable time after midnight or before 6am. Strictly return a list of events that needed to be scheuled to complete the tasks following the Event schema 
+provided above in a valid json format. """Insert scheule here"""
+        `
+    }
+    const completion = await openai.chat.completions.create({
+        messages: [systemPrompt, taskPrompt],
+        model: 'gpt-3.5-turbo',
+    });
+    const response = await completion.choices[0].message.content
+    const eventsToBeScheduled: GoogleEvent[] = response ? JSON.parse(response) : []
+    return eventsToBeScheduled
+}
--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -5,59 +5,67 @@ import {TaskCategory} from "../components/tasks/TaskCategory";
 import {useSession, useSupabaseClient} from "@supabase/auth-helpers-react";
 import {getFutureEvents} from "../api/GoogleCalendar";
 import {GoogleEvent} from "../types/googleapitypes";
-
+import {sendGPT} from "../api/OpenAI";
+const sampleTasks: Task[] = [
+  {
+    title: "Finish Report on Market Trends",
+    tag: "Work",
+    urgency: "High",
+    due: new Date("2023-09-11"),
+    estimateTimeToComplete: 5,
+  },
+  {
+    title: "Buy Groceries",
+    tag: "Personal",
+    urgency: "Medium",
+    due: new Date("2023-09-10"),
+    estimateTimeToComplete: 1.5,
+  },
+  {
+    title: "Plan Family Vacation",
+    tag: "Family",
+    urgency: "Low",
+    due: new Date("2023-09-20"),
+    estimateTimeToComplete: 3,
+  },
+  {
+    title: "Submit Expense Report",
+    tag: "Work",
+    urgency: "Medium",
+    due: new Date("2023-09-12"),
+    estimateTimeToComplete: 1,
+  },
+];
 async function getLatestCalendarEvent(token: string) {
   const events = await getFutureEvents(token);
-  const simplifiedEvents: GoogleEvent = events.map((event: any) => ({
+  console.log(events);
+  const simplifiedEvents: GoogleEvent[] = events.map((event: any) => ({
     summary: event.summary,
     description: event?.description,
     start: event.start,
     end: event.end,
   }));
-  return simplifiedEvents;
+  const response = await sendGPT(sampleTasks, simplifiedEvents);
+  console.log(response);
 }
+
 export function TasksPage() {
   const session = useSession();
   const token = session?.provider_token as string;
-
-  const sampleTasks: Task[] = [
-    {
-      title: "Finish Report on Market Trends",
-      tag: "Work",
-      urgency: "High",
-      due: new Date("2023-09-11"),
-      estimateTimeToComplete: 5,
-    },
-    {
-      title: "Buy Groceries",
-      tag: "Personal",
-      urgency: "Medium",
-      due: new Date("2023-09-10"),
-      estimateTimeToComplete: 1.5,
-    },
-    {
-      title: "Plan Family Vacation",
-      tag: "Family",
-      urgency: "Low",
-      due: new Date("2023-09-20"),
-      estimateTimeToComplete: 3,
-    },
-    {
-      title: "Submit Expense Report",
-      tag: "Work",
-      urgency: "Medium",
-      due: new Date("2023-09-12"),
-      estimateTimeToComplete: 1,
-    },
-  ];
-
-  getLatestCalendarEvent(token);
 
   return (
     <div>
       <TaskCategory tasks={sampleTasks} category="Personal" />
       <TaskCategory tasks={sampleTasks} category="Family" />
       <TaskCategory tasks={sampleTasks} category="Work" />
+
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => getLatestCalendarEvent(token)}
+      >
+        {" "}
+        send to gpt{" "}
+      </button>
     </div>
   );
 }
